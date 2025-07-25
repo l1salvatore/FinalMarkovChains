0eabc4e51b66c42de393a64aa7b5e4a2e409e85a
lsalvatore <lucas_salva_15@hotmail.com>
2025-07-24 18:40:12 -0700

    24-07

--- TPFinalEjercicio2.R
+++ TPFinalEjercicio2.R
@@ -1,19 +1,16 @@
 options(scipen = 999)
 # Cantidad de simulaciones
-n <- 400000
-# Con rbinom generamos 350 'viviendas' con probabilidad 0.37 de tener certificado (1) o de no tener(0)
-# Esas 350 las sumamos para obtener un conteo. Replicamos n veces para obtener un vector conteos
-# Y en base a ese vector o lista, realizamos el gráfico correspondiente
-conteos <- replicate(n, sum(rbinom(350, 1, 0.37)))
 
-# Obtener la tabla de frecuencias
-frecuencias <- table(conteos)
+# Obtener la tabla de frecuencias para rbinom
+frecuencias <- rbinom(400000, 350, 0.37)
+frecuenciasTable <- table(frecuencias)
+
 # Convertir a vectores para graficar con líneas
-x_vals <- as.numeric(names(frecuencias))
-y_vals <- as.numeric(frecuencias)
+x_vals <- as.numeric(names(frecuenciasTable))
+y_vals <- as.numeric(frecuenciasTable)
 # Graficar como curva
 plot(x_vals, y_vals, type = "o", pch = 19, col = "darkgreen",
-     main =  paste("Conteo de número de éxitos en ", n, "repeticiones de rbinom(350, 1, 0.37)"),
-     xlab = "Cantidad de éxitos (1s)", ylab = "Frecuencia (repeticiones)")
+     main =  paste("Número de ensayos de acuerdo al número de viviendas con certificado Re.Na.Ba.P"),
+     xlab = "Cantidad de viviendas con certificado", ylab = "Frecuencia (número de ensayos")
 
-summary(conteos)
+summary(frecuencias)
--- TPFinalEjercicio3.R
+++ TPFinalEjercicio3.R
@@ -7,12 +7,12 @@ ProcesoPois <- function(t, lambda){
 }
 
 # Datos simulados
-N6 <- ProcesoPois(6, 1.5)
+N6 <- ProcesoPois(0.5, 18)
 
 # Construcción de segmentos horizontales
 segmentos <- data.frame(
   x_inicio = N6$x,
-  x_fin = c(N6$x[-1], 6),  # finaliza en 6
+  x_fin = c(N6$x[-1], 0.5),  # finaliza en 0.5
   y = N6$y
 )
 
@@ -21,14 +21,19 @@ ggplot() +
   geom_segment(data = segmentos, aes(x = x_inicio, xend = x_fin, y = y, yend = y),
                color = "red", size = 1) +
   geom_point(data = N6, aes(x = x, y = y), color = "red", size = 2) +
-  scale_x_continuous(name = "Tiempo (meses)",   unique(round(N6$x, 1))) +
+  scale_x_continuous(name = "Tiempo (años)", breaks = round(unique(N6$x), 1)) +
   scale_y_continuous(name = "N(t)", breaks = 0:max(N6$y)) +
-  ggtitle("Proceso de Poisson para los próximos 6 meses") +
-  theme_minimal()
+  ggtitle("Núcleos familiares que llegan para los próximos 6 meses") +
+  theme_minimal() +
+  theme(
+    # Etiquetas de los ejes (los números)
+    axis.text.x = element_text(face = "bold", color = "black", size = 10),
+    axis.text.y = element_text(face = "bold", color = "black", size = 10)
+  )
 
 
 # Datos simulados para 15 años
-N15 <- ProcesoPois(180, 1.5)
+N15 <- ProcesoPois(15, 18)
 # Tiempos entre llegadas
 interarrivals <- diff(N15$x)
 
@@ -36,17 +41,17 @@ summary(interarrivals)
 
 # Histograma con densidad
 hist(interarrivals, breaks = 20, probability = TRUE, col = "skyblue",
-     main = "Histograma de T (tiempos entre llegadas en meses)",
-     xlab = "Tiempo entre llegadas (Meses)")
+     main = "Histograma de T (tiempos entre llegadas en años)",
+     xlab = "Tiempo entre llegadas (Años)")
 
 lines(density(interarrivals), col = "darkblue", lwd = 2)
-curve(dexp(x, rate = 1.5), add = TRUE, col = "red", lwd = 2, lty = 2)
+curve(dexp(x, rate = 18), add = TRUE, col = "red", lwd = 2, lty = 2)
 legend("topright", legend = c("Densidad empírica", "Exponencial teórica"), 
        col = c("darkblue", "red"), lty = c(1, 2), lwd = 2)
 
 
 
 n <- 100000
-medianas <- replicate(n, mean(diff(ProcesoPois(180,1.5)$x)))
+medianas <- replicate(n, mean(diff(ProcesoPois(15,18)$x)))
 summary(medianas)
 
